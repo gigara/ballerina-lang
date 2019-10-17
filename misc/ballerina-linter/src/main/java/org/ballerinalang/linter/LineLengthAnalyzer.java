67901dc106aebac43bfd014f55e4dcc2e906650b
Gigara <info@gigara.info>
2019-10-17 14:05:26 +0530

    Implemented string buffered reader to split the lines instead of split using \n

--- misc/ballerina-linter/src/main/java/org/ballerinalang/linter/LineLengthAnalyzer.java
+++ misc/ballerina-linter/src/main/java/org/ballerinalang/linter/LineLengthAnalyzer.java
@@ -8,20 +8,25 @@ import org.ballerinalang.util.diagnostic.DiagnosticLog;
 import org.wso2.ballerinalang.compiler.util.diagnotic.BDiagnosticSource;
 import org.wso2.ballerinalang.compiler.util.diagnotic.DiagnosticPos;
 
+import java.io.BufferedReader;
+import java.io.StringReader;
+import java.util.concurrent.atomic.AtomicInteger;
+
 class LineLengthAnalyzer {
     static void lintLineLength(JsonObject model, CompilationUnitNode compilationUnitNode, DiagnosticLog dLog) {
         String source = FormattingSourceGen.getSourceOf(model);
-        String[] lines = source.split("\n");
-        for (int i = 0; i < lines.length; i++) {
-            String line = lines[i];
+        AtomicInteger lineNo = new AtomicInteger(1);
+        new BufferedReader(new StringReader(source))
+                .lines().forEach(line -> {
             int length = line.trim().length();
             if (length > 120) {
                 int sCol = line.length() - length + 1;
                 Diagnostic.DiagnosticPosition pos = new DiagnosticPos((BDiagnosticSource) compilationUnitNode.getPosition().getSource(),
-                        i + 1, i + 1, sCol, sCol + length);
+                        lineNo.get(), lineNo.get(), sCol, sCol + length);
 
                 dLog.logDiagnostic(Diagnostic.Kind.WARNING, pos, "Line length should not exceed 120 chars");
             }
-        }
+            lineNo.getAndIncrement();
+        });
     }
 }
