37990c47faa44c98bb4e8a7346fef59e10ebf233
Gigara <info@gigara.info>
2020-01-07 17:30:36 +0530

    Add node position matching node to the unused visitor

--- language-server/modules/langserver-core/src/main/java/org/ballerinalang/langserver/common/unused/UnusedVisitor.java
+++ language-server/modules/langserver-core/src/main/java/org/ballerinalang/langserver/common/unused/UnusedVisitor.java
@@ -1,24 +1,22 @@
 /*
-
-  * Copyright (c) 2019, WSO2 Inc. (http://wso2.com) All Rights Reserved.
-  *
-  * Licensed under the Apache License, Version 2.0 (the "License");
-  * you may not use this file except in compliance with the License.
-  * You may obtain a copy of the License at
-  *
-  * http://www.apache.org/licenses/LICENSE-2.0
-  *
-  * Unless required by applicable law or agreed to in writing, software
-  * distributed under the License is distributed on an "AS IS" BASIS,
-  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-  * See the License for the specific language governing permissions and
-  * limitations under the License.
-
+ * Copyright (c) 2019, WSO2 Inc. (http://wso2.com) All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
-
 package org.ballerinalang.langserver.common.unused;
 
 import org.ballerinalang.langserver.common.LSNodeVisitor;
+import org.ballerinalang.langserver.common.constants.ContextConstants;
 import org.ballerinalang.langserver.common.utils.CommonUtil;
 import org.ballerinalang.langserver.compiler.DocumentServiceKeys;
 import org.ballerinalang.langserver.compiler.LSContext;
@@ -33,14 +31,33 @@ import org.wso2.ballerinalang.compiler.tree.BLangFunction;
 import org.wso2.ballerinalang.compiler.tree.BLangImportPackage;
 import org.wso2.ballerinalang.compiler.tree.BLangNode;
 import org.wso2.ballerinalang.compiler.tree.BLangPackage;
+import org.wso2.ballerinalang.compiler.tree.BLangRecordVariable;
+import org.wso2.ballerinalang.compiler.tree.BLangService;
 import org.wso2.ballerinalang.compiler.tree.BLangSimpleVariable;
+import org.wso2.ballerinalang.compiler.tree.BLangTupleVariable;
 import org.wso2.ballerinalang.compiler.tree.BLangTypeDefinition;
+import org.wso2.ballerinalang.compiler.tree.BLangVariable;
 import org.wso2.ballerinalang.compiler.tree.expressions.BLangConstant;
+import org.wso2.ballerinalang.compiler.tree.expressions.BLangGroupExpr;
+import org.wso2.ballerinalang.compiler.tree.expressions.BLangInvocation;
+import org.wso2.ballerinalang.compiler.tree.expressions.BLangLiteral;
+import org.wso2.ballerinalang.compiler.tree.expressions.BLangRecordVarRef;
+import org.wso2.ballerinalang.compiler.tree.expressions.BLangServiceConstructorExpr;
+import org.wso2.ballerinalang.compiler.tree.expressions.BLangSimpleVarRef;
 import org.wso2.ballerinalang.compiler.tree.statements.BLangBlockStmt;
+import org.wso2.ballerinalang.compiler.tree.statements.BLangForeach;
+import org.wso2.ballerinalang.compiler.tree.statements.BLangIf;
+import org.wso2.ballerinalang.compiler.tree.statements.BLangRecordDestructure;
+import org.wso2.ballerinalang.compiler.tree.statements.BLangRecordVariableDef;
 import org.wso2.ballerinalang.compiler.tree.statements.BLangSimpleVariableDef;
+import org.wso2.ballerinalang.compiler.tree.statements.BLangTransaction;
+import org.wso2.ballerinalang.compiler.tree.statements.BLangTupleVariableDef;
+import org.wso2.ballerinalang.compiler.tree.statements.BLangWhile;
 import org.wso2.ballerinalang.compiler.tree.types.BLangObjectTypeNode;
+import org.wso2.ballerinalang.compiler.tree.types.BLangRecordTypeNode;
 import org.wso2.ballerinalang.compiler.util.diagnotic.DiagnosticPos;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class UnusedVisitor extends LSNodeVisitor {
@@ -70,6 +87,19 @@ public class UnusedVisitor extends LSNodeVisitor {
         node.accept(this);
     }
 
+    private boolean isMatching(DiagnosticPos namePosition) {
+        boolean isMatching = false;
+
+        if (namePosition.sLine == position.getLine()
+                && namePosition.eLine >= position.getLine()
+                && namePosition.sCol <= position.getCharacter()
+                && namePosition.eCol >= position.getCharacter()) {
+            isMatching = true;
+        }
+
+        return isMatching;
+    }
+
     @Override
     public void visit(BLangPackage pkgNode) {
         boolean isTestSrc = CommonUtil.isTestSource(this.context.get(DocumentServiceKeys.RELATIVE_FILE_PATH_KEY));
@@ -93,12 +123,7 @@ public class UnusedVisitor extends LSNodeVisitor {
     @Override
     public void visit(BLangFunction funcNode) {
         // Check for native functions
-        BSymbol funcSymbol = funcNode.symbol;
-        if (Symbols.isNative(funcSymbol) || !CommonUtil.isValidInvokableSymbol(funcSymbol)) {
-            return;
-        }
-
-        if (HoverUtil.isMatchingPosition(funcNode.pos, this.position)) {
+        if (isMatching(funcNode.name.pos)) {
             setTerminateVisitor();
             diagnosticPos = funcNode.pos;
         }
@@ -107,7 +132,8 @@ public class UnusedVisitor extends LSNodeVisitor {
             funcNode.requiredParams.forEach(this::acceptNode);
         }
 
-        if (funcNode.returnTypeNode != null && !terminateVisitor && !(funcNode.returnTypeNode.type instanceof BNilType)) {
+        if (funcNode.returnTypeNode != null && !terminateVisitor &&
+                !(funcNode.returnTypeNode.type instanceof BNilType)) {
             this.acceptNode(funcNode.returnTypeNode);
         }
 
@@ -130,11 +156,56 @@ public class UnusedVisitor extends LSNodeVisitor {
 
     @Override
     public void visit(BLangSimpleVariable varNode) {
+        boolean isFunctionParam = varNode.parent instanceof BLangFunction;
+        boolean isTupleVariable = varNode.parent instanceof BLangTupleVariable;
+        boolean isRecordVariable = varNode.parent instanceof BLangRecordVariable;
+
         if (varNode.symbol != null && !terminateVisitor) {
-            if (HoverUtil.isMatchingPosition(varNode.pos, this.position)) {
+            if (isMatching(varNode.name.pos)) {
                 setTerminateVisitor();
                 diagnosticPos = varNode.pos;
+
+                // change columns if the var is a function parameter
+                if (isFunctionParam || isTupleVariable || isRecordVariable) {
+                    List<? extends BLangVariable> params = null;
+
+                    if (isFunctionParam) {
+                        params = ((BLangFunction) varNode.parent).requiredParams;
+
+                    } else if (isTupleVariable) {
+                        params = ((BLangTupleVariable) varNode.parent).memberVariables;
+
+                    } else if (isRecordVariable) {
+                        List<BLangVariable> variables = new ArrayList<>();
+                        for (BLangRecordVariable.BLangRecordVariableKeyValue variable:((BLangRecordVariable) varNode.parent).variableList) {
+                            variables.add(variable.valueBindingPattern);
+                        }
+                        params = variables;
+                    }
+
+                    for (int i = 0; i < params.size(); i++) {
+                        if (params.get(i) == varNode) {
+                            if (params.size() > 1) {
+                                if (i != params.size() -1) {
+                                    DiagnosticPos nextNodePos = params.get(i+1).pos;
+                                    diagnosticPos.eCol = nextNodePos.sCol;
+
+                                } else {
+                                    DiagnosticPos previousNodePos = params.get(i-1).pos;
+                                    diagnosticPos.sCol = previousNodePos.eCol;
+                                }
+                            }
+                        }
+                    }
+                }
             }
+        }
+        if (varNode.expr != null) {
+            this.acceptNode(varNode.expr);
+        }
+
+        if (varNode.getTypeNode() != null) {
+            this.acceptNode(varNode.getTypeNode());
         }
     }
 
@@ -148,9 +219,10 @@ public class UnusedVisitor extends LSNodeVisitor {
     @Override
     public void visit(BLangConstant constant) {
         if (constant.symbol != null && !terminateVisitor) {
-            if (HoverUtil.isMatchingPosition(constant.pos, this.position)) {
+            if (isMatching(constant.name.pos)) {
                 setTerminateVisitor();
                 diagnosticPos = constant.pos;
+
             }
         }
     }
@@ -158,9 +230,10 @@ public class UnusedVisitor extends LSNodeVisitor {
     @Override
     public void visit(BLangTypeDefinition typeDefinition) {
         if (typeDefinition.symbol != null && !terminateVisitor) {
-            if (HoverUtil.isMatchingPosition(typeDefinition.pos, this.position)) {
+            if (isMatching(typeDefinition.name.pos)) {
                 setTerminateVisitor();
                 diagnosticPos = typeDefinition.pos;
+                return;
             }
         }
 
@@ -170,22 +243,172 @@ public class UnusedVisitor extends LSNodeVisitor {
     }
 
     @Override
+    public void visit(BLangRecordTypeNode recordTypeNode) {
+        if (recordTypeNode.fields != null && !terminateVisitor) {
+            recordTypeNode.fields.forEach(this::acceptNode);
+        }
+
+        if (recordTypeNode.initFunction != null  && !terminateVisitor &&
+                !(recordTypeNode.initFunction.returnTypeNode.type instanceof BNilType)) {
+            this.acceptNode(recordTypeNode.initFunction);
+        }
+    }
+
+    @Override
     public void visit(BLangObjectTypeNode objectTypeNode) {
 
-        if (objectTypeNode.fields != null) {
+        if (objectTypeNode.fields != null && !terminateVisitor) {
             objectTypeNode.fields.forEach(this::acceptNode);
         }
 
-        if (objectTypeNode.functions != null) {
+        if (objectTypeNode.functions != null && !terminateVisitor) {
             objectTypeNode.functions.forEach(this::acceptNode);
         }
 
-        if (objectTypeNode.initFunction != null) {
+        if (objectTypeNode.initFunction != null && !terminateVisitor) {
             this.acceptNode(objectTypeNode.initFunction);
         }
 
-        if (objectTypeNode.receiver != null) {
+        if (objectTypeNode.receiver != null && !terminateVisitor) {
             this.acceptNode(objectTypeNode.receiver);
+        }
+    }
+
+    @Override
+    public void visit(BLangServiceConstructorExpr serviceConstructorExpr) {
+        if (serviceConstructorExpr.serviceNode != null && !terminateVisitor) {
+            this.acceptNode(serviceConstructorExpr.serviceNode);
+        }
+    }
+
+    @Override
+    public void visit(BLangService serviceNode) {
+        if (serviceNode.resourceFunctions != null && !terminateVisitor) {
+            serviceNode.resourceFunctions.forEach(this::acceptNode);
+        }
+    }
+
+    @Override
+    public void visit(BLangTupleVariableDef bLangTupleVariableDef) {
+        if (bLangTupleVariableDef.var != null && !terminateVisitor) {
+            this.acceptNode(bLangTupleVariableDef.var);
+        }
+    }
+
+    @Override
+    public void visit(BLangTupleVariable bLangTupleVariable) {
+        if (bLangTupleVariable.memberVariables != null && !terminateVisitor) {
+            bLangTupleVariable.memberVariables.forEach(this::acceptNode);
+        }
+    }
+
+    @Override
+    public void visit(BLangWhile whileNode) {
+        if (whileNode.expr != null && !terminateVisitor) {
+            this.acceptNode(whileNode.expr);
+        }
+        if (whileNode.body != null && !terminateVisitor) {
+            this.acceptNode(whileNode.body);
+        }
+    }
+
+    @Override
+    public void visit(BLangGroupExpr groupExpr) {
+        if (groupExpr.expression != null && !terminateVisitor) {
+            this.acceptNode(groupExpr.expression);
+        }
+    }
+
+    @Override
+    public void visit(BLangTransaction transactionNode) {
+        if (transactionNode.transactionBody != null && !terminateVisitor) {
+            acceptNode(transactionNode.transactionBody);
+        }
+
+        if (transactionNode.onRetryBody != null && !terminateVisitor) {
+            acceptNode(transactionNode.onRetryBody);
+        }
+
+        if (transactionNode.retryCount != null && !terminateVisitor) {
+            acceptNode(transactionNode.retryCount);
+        }
+    }
+
+    @Override
+    public void visit(BLangIf ifNode) {
+        if (ifNode.expr != null && !terminateVisitor) {
+            acceptNode(ifNode.expr);
+        }
+
+        if (ifNode.body != null && !terminateVisitor) {
+            acceptNode(ifNode.body);
+        }
+
+        if (ifNode.elseStmt != null && !terminateVisitor) {
+            acceptNode(ifNode.elseStmt);
+        }
+    }
+
+    public void visit(BLangForeach foreach) {
+        if (foreach.collection != null && !terminateVisitor) {
+            acceptNode(foreach.collection);
+        }
+
+        acceptNode((BLangNode) foreach.variableDefinitionNode);
+
+        if (foreach.body != null && !terminateVisitor) {
+            acceptNode(foreach.body);
+        }
+    }
+
+    @Override
+    public void visit(BLangRecordVariable bLangRecordVariable) {
+        if (bLangRecordVariable != null) {
+            for (BLangRecordVariable.BLangRecordVariableKeyValue bLangRecordVariableKeyValue :
+                    bLangRecordVariable.variableList) {
+                acceptNode(bLangRecordVariableKeyValue.valueBindingPattern);
+            }
+            acceptNode(bLangRecordVariable.expr);
+        }
+    }
+
+    @Override
+    public void visit(BLangRecordVariableDef bLangRecordVariableDef) {
+        if (bLangRecordVariableDef.var != null && !terminateVisitor) {
+            acceptNode(bLangRecordVariableDef.var);
+        }
+    }
+
+    @Override
+    public void visit(BLangRecordDestructure stmt) {
+        if (stmt.expr != null) {
+            acceptNode(stmt.expr);
+        }
+
+        if (stmt.varRef != null) {
+            acceptNode(stmt.varRef);
+        }
+    }
+
+    @Override
+    public void visit(BLangInvocation invocationExpr) {
+        if (invocationExpr.expr != null && !terminateVisitor) {
+            acceptNode(invocationExpr.expr);
+        }
+
+        if (invocationExpr.argExprs != null && !terminateVisitor) {
+            invocationExpr.argExprs.forEach(this::acceptNode);
+        }
+
+        if (!terminateVisitor && HoverUtil.isMatchingPosition(invocationExpr.getPosition(), this.position)) {
+            setTerminateVisitor();
+        }
+    }
+
+    @Override
+    public void visit(BLangRecordVarRef varRefExpr) {
+        if (varRefExpr.restParam != null && !terminateVisitor) {
+            acceptNode((BLangNode) varRefExpr.restParam);
         }
     }
 
